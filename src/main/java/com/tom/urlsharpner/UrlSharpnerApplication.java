9b17a51024c717ccad0d738e0ede106b6dc2f196
Thomas <thomas1782000t@gmail.com>
2024-05-25 18:12:53 +0530

    Initial commit

---
+++ src/main/java/com/tom/urlsharpner/UrlSharpnerApplication.java
@@ -0,0 +1,13 @@
+package com.tom.urlsharpner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlSharpnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlSharpnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tom/urlsharpner/UrlSharpnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tom.urlsharpner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlSharpnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
